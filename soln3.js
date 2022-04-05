57e600e0100180303f2909e9fae2e128f58d557e
Asim Sapkota <asimsapkota1836@gmail.com>
2022-04-05 10:12:53 +0545

    Problem 3 solved

--- soln3.js
+++ soln3.js
@@ -14,3 +14,23 @@ function isPrime(num) {
 }
 
 console.log(isPrime(process.argv[2]));
+
+function primeFactor(num) {
+	let grtstNum = 0;
+
+	for (let i = 2; i < num; i++) {
+		if (isPrime(num)) {
+			grtstNum = grtstNum > num ? grtstNum : num;
+			break;
+		}
+		if (isPrime(i) && (num % i <= 0)) {
+			grtstNum  = grtstNum > i ? grtstNum : i;
+			num = num / i;
+			i = 2
+		}
+	}
+
+	return grtstNum;
+}
+
+console.log(primeFactor(600851475143));
